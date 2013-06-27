1f195c60264730ee14190cb53fe485dd9f1acac1
dkoppel <admin@administrhater.com>
2013-06-26 20:10:38 -0500

    Fucking egotistical bastard. adds expandtab to vimrc

--- iibot.sh
+++ iibot.sh
@@ -13,20 +13,25 @@
 #|                                |
 #|________________________________|
 
-ii -s irc.freenode.net -n Dav3b0t -i /tmp/ &
+SERVER="irc.freenode.net"
+CHANNEL="#buttsavage"
+NICK="dav3bot"
+
+( 
+flock -n -e 200 || exit 1
+ii -s ${SERVER} -n ${NICK} -i /tmp/& 
+echo $! > /tmp/iibot.pid
+trap 'kill `cat /tmp/iibot.pid`;rm /tmp/iibot.pid' EXIT 
 sleep 1
-echo '/j #buttsavage' > '/tmp/irc.freenode.net/in'
+echo "/j ${CHANNEL}" > /tmp/${SERVER}/in
 sleep 1
-echo '-online-' > '/tmp/irc.freenode.net/#buttsavage/in'
-tailf -n1 '/tmp/irc.freenode.net/#buttsavage/out' | \
+echo "-online-" > /tmp/${SERVER}/${CHANNEL}/in
+tailf -n1 /tmp/${SERVER}/${CHANNEL}/out | \
     while read -r date time nick mesg; do
         case $mesg in
             '!ping')
                 msg=$(echo ${nick}|sed -e 's/>//g' -e 's/<//g')": Pong!"
                 ;;
-            #'!pacman')# this script floods and drops ii.
-            #    exec ~/scripts/pacman-short.sh > /tmp/irc.freenode.net/#buttsavage/in
-            #    ;;
             '!commit')
                 msg=$(curl -s whatthecommit.com/index.txt)
                 ;;
@@ -40,11 +45,11 @@ tailf -n1 '/tmp/irc.freenode.net/#buttsavage/out' | \
                 msg=$(host `echo $mesg | awk '{print $NF}'`)
                 ;;
             '!suicide')
-                [ $nick ==  "<kuroishi>" ] && { echo 'goodbye' > /tmp/irc.freenode.net/#buttsavage/in ; break; }
+                [ $nick ==  "<kuroishi>" ] && { echo 'goodbye' > /tmp/${SERVER}/${CHANNEL}/in ; break; }
                 ;;
         esac
     [[ -n $msg ]] && echo $msg ; msg=''
-done > /tmp/irc.freenode.net/#buttsavage/in
-killall ii
-rm -rf /tmp/irc.freenode.net 
-
+done > /tmp/${SERVER}/${CHANNEL}/in
+kill `cat /tmp/iibot.pid` ; rm /tmp/iibot.pid
+) 200>/var/lock/.ii.lock
+rm -rf /tmp/${SERVER} 
